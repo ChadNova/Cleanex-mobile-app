f96af04974a977bc780abd3679e03d47dd5d8213
Moussassoss <ssossinfo88@gmail.com>
2025-12-31 00:24:40 +0200

    fixed crash android

--- app.config.js
+++ app.config.js
@@ -45,3 +45,60 @@ export default {
     },
   },
 };
+
+
+
+/* 
+
+{
+  "expo": {
+    "name": "CleanEx",
+    "slug": "cleanex-student-app",
+    "version": "1.0.1",
+    "orientation": "default",
+    "icon": "./assets/images/icon.png",
+    "scheme": "cleanex",
+    "userInterfaceStyle": "light",
+    "newArchEnabled": true,
+    "ios": {
+      "supportsTablet": false,
+      "bundleIdentifier": "com.cleanex.student",
+      "infoPlist": {
+        "ITSAppUsesNonExemptEncryption": false,
+        "NSCameraUsageDescription": "CleanEx uses the camera to allow users to upload their profile photos"
+      }
+    },
+    "android": {
+      "package": "com.cleanex.student"
+    },
+    "web": {
+      "bundler": "metro",
+      "output": "single",
+      "favicon": "./assets/images/favicon.png"
+    },
+    "plugins": [
+      "expo-router",
+      "expo-font",
+      "expo-web-browser",
+      [
+        "expo-splash-screen",
+        {
+          "backgroundColor": "#4F46E5",
+          "image": "./assets/images/logo.png",
+          "resizeMode": "contain"
+        }
+      ]
+    ],
+    "experiments": {
+      "typedRoutes": true
+    },
+    "extra": {
+      "router": {},
+      "eas": {
+        "projectId": "aeb1b54c-2061-44fa-81e2-0f5ab0a0ee7f"
+      }
+    }
+  }
+}
+
+*/
